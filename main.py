41bcddb656cd13452c90e81bbb8c15b85a040e04
kamikotoin <166481079+kamikotoin@users.noreply.github.com>
2026-01-13 23:00:30 +0500

    shipe_moveset
    ship now can move, and window has a border that ship cannot cross

--- main.py
+++ main.py
@@ -15,20 +15,57 @@ class AlienInvasion:
         
         pygame.display.set_caption("Alien invasion")
 
-        self.ship = Ship(self.screen)
+        self.ship = Ship(self)
 
     def run_game(self):
         """Запуск основного цикла игры"""
         while True:
-            #event listener
+            #checks for input
+            self._check_events()
+            #creating everything on screen
+            self._update_screen()
+            #update ship position
+            self.ship.update()
+
+
+    def _check_events(self):
+        #event listener
             for event in pygame.event.get():
                 if event.type==pygame.QUIT:
                     sys.exit()
+                elif event.type==pygame.KEYDOWN:
+                    self._check_keydown_events(event)
+                elif event.type==pygame.KEYUP:
+                    self._check_keyup_events(event)
+                    
+
+    def _check_keydown_events(self, event):
+        if event.key==pygame.K_RIGHT:
+            #move to right
+            self.ship.moving_right=True
+        elif event.key==pygame.K_LEFT:
+            #move to left
+            self.ship.moving_left=True
+        elif event.key==pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key==pygame.K_RIGHT:
+            #stop move to right
+            self.ship.moving_right=False
+        elif event.key==pygame.K_LEFT:
+            #stop move to left
+            self.ship.moving_left=False
+        
+
+
+    def _update_screen(self):
             #при каждом переходе меняет цвет на этот:
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
             #Отображение последнего прорисованного экрана
             pygame.display.flip()
+
 
 if __name__ =="__main__":
    #создание экзампяра и запуска игры
--- ship.py
+++ ship.py
@@ -6,12 +6,33 @@ class Ship():
         """Инициализирует корабль и задает его начальную позицую"""
         self.screen = ai_game.screen
         self.screen_rect =ai_game.screen.get_rect()
+        self.settings = ai_game.settings
         
         #Загружает изображения корабля и получает прямоугольник
-        self.image=pygame.image.load("images/ship.bmp")
+        self.image=pygame.image.load("alien invasion/alien-invasion/images/ship.bmp")
         self.rect = self.image.get_rect()
+
         #Каждый новый корабль повляется у нижнего экрана
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #cordinate of ship 
+        self.x=float(self.rect.x)
+
+        #is moving or not
+        self.moving_right=False
+        self.moving_left=False
+        
+
+
+    def update(self):\
+        #updates coordinates x
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x+=self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x-=self.settings.ship_speed
+        #updates ingame coordinates
+        self.rect.x=self.x
+        
 
     def blitme(self):
         """Рисует корабль в текущей позиции"""
